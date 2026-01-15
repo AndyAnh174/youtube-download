a9640f6582079db398ff54317bc3e1a94e26bcf4
AndyAnh174 <hovietanh147@gmail.com>
2026-01-15 08:57:37 +0700

    feat: Replace Django's `static` helper with a manual `re_path` and `serve` view for media file serving.

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
